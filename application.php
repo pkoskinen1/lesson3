c211730dba50dad8dc2b3dfbef98718dbaa65aa3
piekos <piekos@greeny.cs.tlu.ee>
2016-02-18 11:00:00 +0200

    save the file

---
+++ application.php
@@ -0,0 +1,19 @@
+<?php
+
+
+
+?>
+
+<h2> First Applications </h2>
+
+
+
+
+
+<form>
+
+<form method="get">
+	<label for="Message">Message:* <label><br>
+	<input type="text" name="Message"<br>
+	<input type="Submit" value="Save to DB">
+<form>
